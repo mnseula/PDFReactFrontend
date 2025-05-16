f30afe799e39cd5141e8b668af6f1561eb17876a
mnseula <mcnseula@gmail.com>
2025-05-15 22:36:44 -0400

    Create constants.js
    // constants.js

---
+++ constants.js
@@ -0,0 +1,18 @@
+// constants.js
+export const toolModes = {
+  VIEW: 'view',
+  CROP: 'crop',
+  ANNOTATE: 'annotate',
+  REDACT: 'redact',
+  COMPRESS: 'compress',
+  OCR: 'ocr',
+  WATERMARK: 'watermark',
+};
+
+export const fileTypes = {
+  PDF: 'pdf',
+  WORD: 'word',
+  EXCEL: 'excel',
+  POWERPOINT: 'powerpoint',
+  IMAGE: 'image',
+};
